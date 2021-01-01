b5c13a73d90514a3c6dafd416c6568868c45759c
derekwilliamson22 <derekwilliamson22@gmail.com>
2021-01-01 01:42:15 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,778 @@
+console.log("hello world!");
+
+const acesHighDeck = 
+[
+    {
+        name: "Two",
+        suit: 'Hearts',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Hearts',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Hearts',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Hearts',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Hearts',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Hearts',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Hearts',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Hearts',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Hearts',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Hearts',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Hearts',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Hearts',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Hearts',
+        value: 14
+    },
+    {
+        name: "Two",
+        suit: 'Spades',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Spades',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Spades',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Spades',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Spades',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Spades',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Spades',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Spades',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Spades',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Spades',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Spades',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Spades',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Spades',
+        value: 14
+    },
+    {
+        name: "Two",
+        suit: 'Diamonds',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Diamonds',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Diamonds',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Diamonds',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Diamonds',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Diamonds',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Diamonds',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Diamonds',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Diamonds',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Diamonds',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Diamonds',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Diamonds',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Diamonds',
+        value: 14
+    },
+        {
+        name: "Two",
+        suit: 'Clubs',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Clubs',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Clubs',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Clubs',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Clubs',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Clubs',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Clubs',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Clubs',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Clubs',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Clubs',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Clubs',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Clubs',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Clubs',
+        value: 14
+    },
+]
+
+const acesLowDeck = 
+[
+    {
+        name: "Two",
+        suit: 'Hearts',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Hearts',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Hearts',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Hearts',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Hearts',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Hearts',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Hearts',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Hearts',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Hearts',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Hearts',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Hearts',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Hearts',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Hearts',
+        value: 1
+    },
+    {
+        name: "Two",
+        suit: 'Spades',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Spades',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Spades',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Spades',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Spades',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Spades',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Spades',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Spades',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Spades',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Spades',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Spades',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Spades',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Spades',
+        value: 1
+    },
+    {
+        name: "Two",
+        suit: 'Diamonds',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Diamonds',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Diamonds',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Diamonds',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Diamonds',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Diamonds',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Diamonds',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Diamonds',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Diamonds',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Diamonds',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Diamonds',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Diamonds',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Diamonds',
+        value: 1
+    },
+        {
+        name: "Two",
+        suit: 'Clubs',
+        value: 2
+    },
+    {
+        name: "Three",
+        suit: 'Clubs',
+        value: 3
+    }, 
+    {
+        name: "Four",
+        suit: 'Clubs',
+        value: 4
+    },    
+    {
+        name: "Five",
+        suit: 'Clubs',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Clubs',
+        value: 6
+    }, 
+    {
+        name: "Seven",
+        suit: 'Clubs',
+        value: 7
+    },
+    {
+        name: "Eight",
+        suit: 'Clubs',
+        value: 8
+    },
+    {
+        name: "Nine",
+        suit: 'Clubs',
+        value: 9
+    }, 
+    {
+        name: "Ten",
+        suit: 'Clubs',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Clubs',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Clubs',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Clubs',
+        value: 13
+    },
+    {
+        name: "Ace",
+        suit: 'Clubs',
+        value: 1
+    },
+]
+
+function getRandomInt(min, max) { 
+    
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+
+// from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleDeck(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+ 
+
+let playerOneCards;
+let playerTwoCards;
+function dealTheCards(array){
+    shuffleDeck(array);
+    const halfTheDeck = Math.ceil(array.length/2);
+    playerOneCards = array.splice(0, halfTheDeck);
+    playerTwoCards = array.splice(-halfTheDeck);
+    return playerOneCards, playerTwoCards; 
+}
+
+const testCards1 = 
+[
+    {
+        name: "Three",
+        suit: 'Clubs',
+        value: 3
+    }, 
+    {
+        name: "Six",
+        suit: 'Clubs',
+        value: 6
+    },
+    {
+        name: "Ten",
+        suit: 'Clubs',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Clubs',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Clubs',
+        value: 12
+    }, 
+    {
+        name: "King",
+        suit: 'Clubs',
+        value: 13
+    }
+]   
+
+const testCards2 = 
+[
+    {
+        name: "Five",
+        suit: 'Clubs',
+        value: 5
+    },
+    {
+        name: "Six",
+        suit: 'Clubs',
+        value: 6
+    },
+    {
+        name: "Six",
+        suit: 'Clubs',
+        value: 6
+    },
+    {
+        name: "Ten",
+        suit: 'Clubs',
+        value: 10
+    },
+    {
+        name: "Jack",
+        suit: 'Clubs',
+        value: 11
+    },
+    {
+        name: "Queen",
+        suit: 'Clubs',
+        value: 12
+    }
+
+]
+function gameOfWar(playerOne, playerTwo){ // where playerOne and playerTwo are both arrays dervied from a standard deck of cards (52), aces are high
+    dealTheCards(acesHighDeck);
+    singleHandTest(playerOneCards, playerTwoCards);
+    //singleHandTest(testCards1, testCards2);
+    if(playerOne && playerTwo !== undefined){
+        console.log("Play the game!");
+    }
+    else {
+        console.log("Please enter two names to play.");
+    }
+}
+
+let array1 = [4,3,2,1,5,4,5,4,4,6];
+let array2 = [4,5,4,4,6,4,3,2,1,5];
+function singleHandTest(a, b){
+    console.log("this is player 1", a);
+    console.log("this is player 2", b);
+    let warHand = [];
+    let warFlop1;
+    let warFlop2;
+    let warFlop1Value;
+    let warFlop2Value;
+    let count = 0;
+    if(a.length === b.length){
+        console.log("both sides have the same number of cards");
+        while(a.length || b.length > 0){
+
+            if(a.length === 0){
+                console.log("player 2 wins");
+                break;
+            }
+            if(b.length === 0){
+                console.log("player 1 wins");
+                break;
+            }
+            let firstElement = a.shift();
+            let secondElement = b.shift();
+            let topCardOneValue = firstElement.value;
+            let topCardTwoValue = secondElement.value;
+            console.log("Player 1 plays:", firstElement);
+            console.log("Player 2 plays:", secondElement);
+            console.log("topCardOneValue:", topCardOneValue);
+            console.log("topCardTwoValue:", topCardTwoValue);
+            if(topCardOneValue > topCardTwoValue){
+                console.log("Player 1's card is higher");
+                a.push(firstElement);
+                a.push(secondElement);
+            }
+            else if(topCardOneValue < topCardTwoValue){
+                console.log("Player 2's card is higher");
+                b.push(firstElement);
+                b.push(secondElement);
+            }
+
+            if(topCardOneValue === topCardTwoValue){
+                console.log("the cards are equal");
+                warHand.push(firstElement);
+                warHand.push(secondElement);
+                warHand.push(a.splice(0,3));
+                warHand.push(b.splice(0,3));
+                console.log("this is the war hand result:", warHand);
+                if(a.length === 0){
+                    console.log("player 2 wins");
+                    return;
+                }
+                if(b.length === 0){
+                    console.log("player 1 wins");
+                    return;
+                }
+                warFlop1 = a.shift();
+                warFlop2 = b.shift();
+                warFlop1Value = warFlop1.value 
+                warFlop2Value = warFlop2.value 
+                console.log("this is the war flop 1:", warFlop1);
+                console.log("this is the war flop 2:", warFlop2);
+                console.log("this is the war flop 1 value:", warFlop1Value);
+                console.log("this is the war flop 2 value:", warFlop2Value);
+                if(warFlop1Value > warFlop2Value){
+                    warHand.push(warFlop1);
+                    warHand.push(warFlop2);
+                    a = a.concat(warHand.flat());
+                    warHand = [];
+                }
+                else if(warFlop1Value < warFlop2Value){
+                    warHand.push(warFlop1);
+                    warHand.push(warFlop2);
+                    b = b.concat(warHand.flat());
+                    warHand = [];
+                }
+                else if(warFlop1Value === warFlop2Value){
+                    warHand.push(firstElement);
+                    warHand.push(secondElement);
+                    warHand.push(a.splice(0,3));
+                    warHand.push(b.splice(0,3));
+                    console.log("this is the war hand result:", warHand);
+                    if(a.length === 0){
+                        console.log("player 2 wins");
+                        return;
+                    }
+                    if(b.length === 0){
+                        console.log("player 1 wins");
+                        return;
+                    }
+                    warFlop1 = a.shift();
+                    warFlop2 = b.shift();
+                    warFlop1Value = warFlop1.value 
+                    warFlop2Value = warFlop2.value 
+                    console.log("this is the war flop 1:", warFlop1);
+                    console.log("this is the war flop 2:", warFlop2);
+                    console.log("this is the war flop 1 value:", warFlop1Value);
+                    console.log("this is the war flop 2 value:", warFlop2Value);
+                    if(warFlop1Value > warFlop2Value){
+                        warHand.push(warFlop1);
+                        warHand.push(warFlop2);
+                        a = a.concat(warHand.flat());
+                        warHand = [];
+                    }
+                    else if(warFlop1Value < warFlop2Value){
+                        warHand.push(warFlop1);
+                        warHand.push(warFlop2);
+                        b = b.concat(warHand.flat());
+                        warHand = [];
+                    }
+                }  
+                console.log("this is the war hand result:", warHand);
+                console.log("this is the new a:", a);
+                console.log("this is the new b:", b);
+            }
+           
+            
+            count ++
+            console.log("Round:",count);
+            console.log("Total Cards:", a.length + b.length);
+            console.log("this is player 1", a);
+            console.log("this is player 2", b);
+        }
+    }
+    else {
+        return "Please reshuffle and start a new game."
+    }
+}
